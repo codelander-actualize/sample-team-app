3c3d742036863586d31ad516f60549683c86f3fa
dani <dani.zaghian@gmail.com>
2019-03-11 10:38:36 -0700

    adds show action

--- app/controllers/api/pages_controller.rb
+++ app/controllers/api/pages_controller.rb
@@ -4,4 +4,8 @@ class Api::PagesController < ApplicationController
 		render json: {message: "Hello! Rachel waz here.", name: "Dani"}
 	end
 
+	def show
+		render json: {message: "This is the show action for the branches demo"}
+	end
+
 end
